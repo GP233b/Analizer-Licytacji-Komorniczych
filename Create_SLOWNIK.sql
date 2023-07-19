b3b8df37f53bf581a9ab15f22ea4c0d5503ee484
Grzegorz Piasny <106985215+GP233b@users.noreply.github.com>
2023-07-19 17:33:03 +0200

    Add files via upload

---
+++ Create_SLOWNIK.sql
@@ -0,0 +1,17 @@
+USE [Baza_licytacji_lasow]
+GO
+
+/****** Object:  Table [dbo].[SLOWNIK]    Script Date: 19.07.2023 17:31:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SLOWNIK](
+	[SLW_ID] [int] NOT NULL,
+	[SLW_SLOWO] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
